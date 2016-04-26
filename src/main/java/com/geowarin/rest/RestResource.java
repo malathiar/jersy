1bd35b0a1564031b7bfaaef991a2b30e6fff59a7
Malathi <malathiapsg@gmail.com>
2016-04-26 11:26:22 +0530

    Update RestResource.java

--- src/main/java/com/geowarin/rest/RestResource.java
+++ src/main/java/com/geowarin/rest/RestResource.java
@@ -22,7 +22,7 @@ public class RestResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/hello")
     public String hello() {
-        return "Hello World";
+        return "Hello World***";
     }
 
     @GET
